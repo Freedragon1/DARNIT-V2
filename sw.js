f24285cb639606a74e2adc7c7b8c92aa58d82cc0
Freedragon1 <freedragon1@hotmail.co.uk>
2026-01-28 14:34:37 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,71 +1,90 @@
-const CACHE_NAME = "darnit-offline-v1";
+/* Darnit2 Service Worker: offline + instant start */
 
-function allCardUrls(){
-  const ranks = ["A","2","3","4","5","6","7","8","9","0","J","Q","K"];
-  const suits = ["S","H","D","C"];
-  const urls = [];
-  for(const r of ranks){
-    for(const s of suits){
-      urls.push(`cards/${r}${s}.png`);
+const CACHE_NAME = "darnit2-cache-v1";
+
+// Build 52 card filenames programmatically (AS..KS, 0H etc)
+const VALUES = ["A","2","3","4","5","6","7","8","9","0","J","Q","K"]; // 10 is "0"
+const SUITS  = ["S","H","D","C"];
+
+function buildCardList(){
+  const out = [];
+  for(const s of SUITS){
+    for(const v of VALUES){
+      out.push(`cards/${v}${s}.png`);
     }
   }
-  return urls;
+  out.push("cards/back.png");
+  return out;
 }
 
-const CORE = [
+const ASSETS = [
   "./",
-  "./index.html",
-  "./manifest.json",
-  "./sw.js",
+  "index.html",
+  "manifest.json",
 
-  "./img/darnitlogo.jpg",
-  "./img/darnitrules.jpg",
-  "./img/gridfull3.jpg",
-  "./img/youlosegrid.jpg",
-  "./img/king.jpg",
-  "./img/queen.jpg",
-  "./img/jack.jpg",
-  "./img/youwin2.jpg",
+  // images
+  "img/darnitlogo.jpg",
+  "img/darnitrules.jpg",
+  "img/gridfull3.jpg",
+  "img/youlosegrid.jpg",
+  "img/king.jpg",
+  "img/queen.jpg",
+  "img/jack.jpg",
+  "img/youwin2.jpg",
 
-  "./sounds/shuffle.mp3",
-  "./sounds/clear.ogg",
-  "./sounds/boing.ogg",
-  "./sounds/lock.ogg",
-  "./sounds/lose.mp3",
-  "./sounds/fanfare.mp3",
-  "./sounds/crowd.mp3",
+  // sounds
+  "sfx/shuffle.mp3",
+  "sfx/clear.ogg",
+  "sfx/boing.ogg",
+  "sfx/lock.ogg",
+  "sfx/lose.mp3",
+  "sfx/fanfare.mp3",
+  "sfx/crowd.mp3",
 
-  "./cards/back.png",
-  ...allCardUrls(),
-];
+  // icons (adjust if your filenames differ)
+  "icons/icon-192.png",
+  "icons/icon-512.png",
+  "icons/icon-180.png",
+  "icons/icon-167.png",
+  "icons/icon-152.png",
+  "icons/icon-120.png",
+  "icons/icon-76.png",
+  "icons/icon-60.png",
+].concat(buildCardList());
 
 self.addEventListener("install", (event) => {
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    await cache.addAll(CORE);
-    self.skipWaiting();
-  })());
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil((async () => {
-    const keys = await caches.keys();
-    await Promise.all(keys.map(k => (k !== CACHE_NAME) ? caches.delete(k) : null));
-    self.clients.claim();
-  })());
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
 });
 
+// Cache-first for instant offline
 self.addEventListener("fetch", (event) => {
-  const url = new URL(event.request.url);
-  if(url.origin !== self.location.origin) return;
+  const req = event.request;
+  if(req.method !== "GET") return;
 
-  event.respondWith((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    const cached = await cache.match(event.request);
-    if(cached) return cached;
-
-    const res = await fetch(event.request);
-    cache.put(event.request, res.clone());
-    return res;
-  })());
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      if(cached) return cached;
+      return fetch(req).then((res) => {
+        // Save a copy for future offline runs
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(req, copy)).catch(()=>{});
+        return res;
+      }).catch(() => {
+        // fallback (if index cached, it will be returned by match above)
+        return cached;
+      });
+    })
+  );
 });
